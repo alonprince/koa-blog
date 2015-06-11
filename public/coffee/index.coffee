b8017e297ab4db84d8ce271708533534627dd9bb
Phishing <alonprince@163.com>
2015-06-11 19:39:15 +0800

    scaffolding

--- public/coffee/index.coffee
+++ public/coffee/index.coffee
@@ -3,15 +3,20 @@ Phishing.main = do ->
     init = ->
         # 滚动条事件
         $('body').on 'wheel', (e) ->
-            $cuTarget = $ e.currentTarget
-            scrollTop = $cuTarget.scrollTop()
-            headerHeight = $ '#header' 
-                            .height()
-            percent = scrollTop / headerHeight
-            percent = 1 if percent > 1
-            opcity = percent * 0.7 + 0.2
-            $ '#header .shadow'
-                .css 'background', "rgba(0, 0, 0, #{opcity})"
+            # 头部背景
+            do ->
+                $cuTarget = $ e.currentTarget
+                scrollTop = $cuTarget.scrollTop()
+                headerHeight = $ '#header' 
+                                .height()
+                percent = scrollTop / headerHeight
+                percent = 1 if percent > 1
+                opcity = percent * 0.7 + 0.2
+                $ '#header .shadow'
+                    .css 'background', "rgba(0, 0, 0, #{opcity})"
+
+            # 文章动画
+            do ->
 
     return {
         init: init
--- public/js/index.js
+++ public/js/index.js
@@ -5,16 +5,19 @@
     var init;
     init = function() {
       return $('body').on('wheel', function(e) {
-        var $cuTarget, headerHeight, opcity, percent, scrollTop;
-        $cuTarget = $(e.currentTarget);
-        scrollTop = $cuTarget.scrollTop();
-        headerHeight = $('#header').height();
-        percent = scrollTop / headerHeight;
-        if (percent > 1) {
-          percent = 1;
-        }
-        opcity = percent * 0.7 + 0.2;
-        return $('#header .shadow').css('background', "rgba(0, 0, 0, " + opcity + ")");
+        (function() {
+          var $cuTarget, headerHeight, opcity, percent, scrollTop;
+          $cuTarget = $(e.currentTarget);
+          scrollTop = $cuTarget.scrollTop();
+          headerHeight = $('#header').height();
+          percent = scrollTop / headerHeight;
+          if (percent > 1) {
+            percent = 1;
+          }
+          opcity = percent * 0.7 + 0.2;
+          return $('#header .shadow').css('background', "rgba(0, 0, 0, " + opcity + ")");
+        })();
+        return (function() {})();
       });
     };
     return {
