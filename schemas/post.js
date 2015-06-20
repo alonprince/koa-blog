56e9751c476c8bced5455a5ecf233b0b5af1833b
Phishing <alonprince@163.com>
2015-06-20 11:48:57 +0800

    edit schema

--- schemas/post.js
+++ schemas/post.js
@@ -2,10 +2,30 @@ var Schema = require('mongoose').Schema;
 
 var Post = new Schema({
     no: { tpye: Number, min: 0 },
-    content: { type: String },
-    created: { type: Date, default: Date.now() },
+    content: String,
+    created: Date,
     history: { type: Array, default:[] },
-    updated: { type: Date }
+    updated: Date
 })
+
+Post.pre('save', function (next) {
+    if (this.isNew) {
+        this.created = this.updated = new Date();
+    }else {
+        this.updated = new Date();
+    };
+    next();
+})  
+
+Post.statics = {
+    find: function(query, cb) {
+        query = query || {};
+        return this.find(query)
+                .sort({
+                    created: -1
+                })
+                .exec(cb)
+    }
+}
 
 module.exports = Post;
